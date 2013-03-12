571783c6c498aa4d158e198034935eee442ea6ab
DAvid Nuon <davidnuongm@gmail.com>
2013-03-11 21:00:27 -0700

    Abstracted NANANANNA

--- main.py
+++ main.py
@@ -1,37 +1,22 @@
 #!/usr/bin/env python 
 #-*- coding: utf-8 -*-  
 
-def main():
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
-    print "NANANANANANANANAN BATMAN!"
+def foo(s):
+    print ("NA"*10) + s 
 
+def main():
+    foo(" BATMAN!")
+    foo(" BATMAN!")
+    foo(" BATMAN!")
+    foo(" BATMAN!")
+    foo(" BATMAN!")
+    foo(" BATMAN!")
+    foo(" BATMAN!")
+    foo(" BATMAN!")
+    foo(" BATMAN!")
+    foo(" BATMAN!")
+    foo(" BATMAN!")
+    foo(" BATMAN!")
 
 if __name__ == '__main__':
     main()
